0662af6bdefa13c87b4f73583a3a48c4a4378896
Harrison Reid <harrison@twosmalltrees.com>
2019-01-06 19:34:38 +1100

    Test fix for render problems...

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-export SortableContainer from './SortableContainer';
-export SortableElement from './SortableElement';
-export SortableHandle from './SortableHandle';
+export SortableContainer from "./SortableContainer";
+export SortableElement from "./SortableElement";
+export SortableHandle from "./SortableHandle";
 
-export sortableContainer from './SortableContainer';
-export sortableElement from './SortableElement';
-export sortableHandle from './SortableHandle';
+export sortableContainer from "./SortableContainer";
+export sortableElement from "./SortableElement";
+export sortableHandle from "./SortableHandle";
 
-export {arrayMove} from './utils';
+export { arrayMove } from "./utils";
